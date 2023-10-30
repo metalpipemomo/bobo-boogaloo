5907d1e97282eb719d3c6785c6dddeee2f54147c
metalpipemomo <bydn.mo@gmail.com>
2023-10-30 01:26:49 -0700

    Tiny fix to clean script.

--- clean.sh
+++ clean.sh
@@ -4,10 +4,8 @@
 BUILD_DIR="./build"
 
 # Check if the build directory exists
-if [ ! -d "$BUILD_DIR" ]; then
-    echo "Nothing to clean; $BUILD_DIR directory does not exist."
-    exit 1
+if [ -d "$BUILD_DIR" ]; then
+    # Remove build dir
+    rm -rf "$BUILD_DIR"
 fi
 
-# Remove build dir
-rm -rf "$BUILD_DIR"
